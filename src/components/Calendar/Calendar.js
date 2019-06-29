e885ccd58dbb3e306dd62d7b420d87c3448c8714
Max Salo <makssalo13@gmail.com>
2019-06-29 18:55:17 +0700

    Calendar

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -8,7 +8,7 @@ class DataItem extends React.Component{
     this.state = {
       flaged: false,
       disabled: false,
-      note: "Blina"
+      note: ""
     }
   }
   
@@ -28,109 +28,119 @@ class DataItem extends React.Component{
     return(
       <td>
         <div className={Name} onClick={this.setFlaged}>
-          <span className="dateDay" data-title={this.props.day}>{this.state.note}</span>
+          <div className="dateDay" >{this.props.day}</div>
         </div>
       </td>
     );
+    }
   }
-  }
 
-
-
-
-  function Month(props){
-
-    let date = props.date;
-    date.setDate(1);
-    let weekDay  = date.getDay();
-    date = new Date(date.getFullYear(), date.getMonth()+1, 0);
-    let daysNum = date.getDate();
     
-
-    const weeks = 6;
-    const days = 7;
-    let day = 0;
-    let tableWeeks = [], tableDays = [];
-    tableWeeks.push(
-      
-    );
-    
-    for(let l =0 ; l<weeks; l++){
-      tableDays = [];
-      for(let i=0; i<days; i++){
-        if(i<weekDay||day>=daysNum){         
-          tableDays.push(
-            <DataItem disabled={true}/>
-          );
+ 
+class Calendar extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            date: new Date()
+            
         }
+    }
+
+    months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+    getDaysNum = () =>{
+        let date = new Date(this.state.date.getFullYear(), this.state.date.getMonth()+1, 0);
         
-
-        else {
-          weekDay=0;
-          day++;
-          tableDays.push(
-          <DataItem day={day}/>
-        );}
-      }
-      tableWeeks.push(<tr> {tableDays} </tr>)
+        return date.getDate();
     }
 
-    return(
-      
-      <tbody>{tableWeeks}</tbody>
-    
-    );  }
+    getFirstWeekDay = () => {
+        let date = this.state.date;
+        date.setDate(1);
+        return date.getDay();
+    }
 
+    getMonthName = () => {
+        return this.months[this.state.date.getMonth()];
+    }
+
+    nextMonth = () =>{
+            
+        this.setState({
+            date: new Date(this.state.date.getFullYear(), this.state.date.getMonth() + 1, 1)
+        });
+        
+        
+        
+    }
+
+    prevMonth = () =>{
+            
+        this.setState({
+            date: new Date(this.state.date.getFullYear(), this.state.date.getMonth() - 1, 1)
+        });
+        
+        
+    }
+
+    buildMonthTable = () =>{
+        let weekDay = this.getFirstWeekDay();
+        let daysNum = this.getDaysNum();
+        const weeks = 6;
+        const days = 7;
+        let day = 0;
+        let tableWeeks = [], tableDays = [];
+        
     
-function NavCalendar(props){
-  return(
-    <div className="navCalendar">
-      <div className="prevMonth"><button onClick={props.prevMonth}/></div>
-      <div className="monthName">{state.monthName}</div>
-      <div className="nextMonth"><button onClick={props.nextMonth}/></div>
-    </div>
-  );
+        for(let l =0 ; l<weeks; l++){
+            tableDays = [];
+            for(let i=0; i<days; i++){
+                if(i<weekDay||day>=daysNum){         
+                    tableDays.push(
+                        <DataItem disabled={true}/>
+                    );
+                }
+                else {
+                    weekDay=0;
+                    day++;
+                    tableDays.push(
+                    <DataItem day={day}/>
+                    );
+                }
+            }
+            tableWeeks.push(<tr> {tableDays} </tr>)
+        }
+
+        return tableWeeks;
+    }
+
+    render(){
+
+        return(
+                <div className="calendarContainer">
+                    <div className="navCalendar">
+                        <button className="prevMonthBtn calendarBtn" onClick={this.prevMonth} >◄</button>
+                        <div className="monthName">{this.getMonthName()}</div>
+                        <button className="nextMonthBtn calendarBtn" onClick={this.nextMonth}>►</button>
+                    </div>
+                    <table>
+                        <thead className="calendarHead">
+                          <tr>
+                            <td>Sun</td>
+                            <td>Mon</td>
+                            <td>Tue</td>
+                            <td>Wed</td>
+                            <td>Thu</td>
+                            <td>Fri</td>
+                            <td>Sat</td>
+                          </tr>
+                        </thead>
+                        <tbody>{this.buildMonthTable()}</tbody>
+                    </table>
+
+                </div>
+          );
+    }
 }
-    
-function Calendar(){
-    const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-    let date = new Date();
-    let currentMonth = date.getMonth();
-    
-    let prevMonth = () =>{
-      (currentMonth<=0) ? currentMonth=11 : currentMonth -=1;
-    }
-
-    let nextMonth = () =>{
-      (currentMonth>=11) ? currentMonth=0 : currentMonth +=1;
-    }
-
-    let setMonth = () =>{
-      setState({
-        monthName: months[currentMonth]
-      });
-    }
-
-    return(
-      <div className="calendarContainer">
-        <NavCalendar monthName={months[currentMonth]} prevMonth={prevMonth} nextMonth={nextMonth}/>
-        <table>
-          <thead className="calendarHead">
-            <tr>
-              <td>Sun</td>
-              <td>Mon</td>
-              <td>Tue</td>
-              <td>Wed</td>
-              <td>Thu</td>
-              <td>Fri</td>
-              <td>Sat</td>
-            </tr>
-          </thead>
-          <Month date={date}/>
-        </table>
-      
-      </div>
-    );
-  }
 
   export default Calendar; 
